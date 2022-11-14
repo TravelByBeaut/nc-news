22ac653e12b7571227e886bc90fe6d90e698a516
TravelByBeaut <ebeaumont77@gmail.com>
2022-11-14 16:57:24 +0000

    adding updated hosting for backend link

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchArticles = ({ topic, sort_by, order_by }) => {
   return axios
-    .get(`https://emily-news-app.herokuapp.com/api/articles`, {
+    .get(`https://emily-backend-news-app.cyclic.app/api/articles`, {
       params: {
         topic,
         sort_by,
@@ -16,7 +16,7 @@ export const fetchArticles = ({ topic, sort_by, order_by }) => {
 
 export const fetchTopics = () => {
   return axios
-    .get(`https://emily-news-app.herokuapp.com/api/topics`)
+    .get(`https://emily-backend-news-app.cyclic.app/api/topics`)
     .then((res) => {
       return res;
     });
@@ -24,7 +24,7 @@ export const fetchTopics = () => {
 
 export const fetchArticleById = (article_id) => {
   return axios
-    .get(`https://emily-news-app.herokuapp.com/api/articles/${article_id}`)
+    .get(`https://emily-backend-news-app.cyclic.app/api/articles/${article_id}`)
     .then(({ data }) => {
       return data;
     });
@@ -32,9 +32,12 @@ export const fetchArticleById = (article_id) => {
 
 export const updateVotes = (article_id, votes) => {
   return axios
-    .patch(`https://emily-news-app.herokuapp.com/api/articles/${article_id}`, {
-      inc_votes: votes,
-    })
+    .patch(
+      `https://emily-backend-news-app.cyclic.app/api/articles/${article_id}`,
+      {
+        inc_votes: votes,
+      }
+    )
     .then(({ data }) => {
       return data;
     });
@@ -43,7 +46,7 @@ export const updateVotes = (article_id, votes) => {
 export const fetchCommentsById = (article_id) => {
   return axios
     .get(
-      `https://emily-news-app.herokuapp.com/api/articles/${article_id}/comments`
+      `https://emily-backend-news-app.cyclic.app/api/articles/${article_id}/comments`
     )
     .then(({ data }) => {
       return data;
@@ -53,7 +56,7 @@ export const fetchCommentsById = (article_id) => {
 export const sendComment = (article_id, username, body) => {
   return axios
     .post(
-      `https://emily-news-app.herokuapp.com/api/articles/${article_id}/comments`,
+      `https://emily-backend-news-app.cyclic.app/api/articles/${article_id}/comments`,
       {
         username,
         body,
@@ -66,13 +69,13 @@ export const sendComment = (article_id, username, body) => {
 
 export const deleteComment = (comment_id) => {
   return axios.delete(
-    `https://emily-news-app.herokuapp.com/api/comments/${comment_id}`
+    `https://emily-backend-news-app.cyclic.app/api/comments/${comment_id}`
   );
 };
 
 export const fetchUsers = () => {
   return axios
-    .get("https://emily-news-app.herokuapp.com/api/users")
+    .get("https://emily-backend-news-app.cyclic.app/api/users")
     .then(({ data }) => {
       return data;
     });
